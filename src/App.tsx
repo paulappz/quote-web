7548704f60eb3b3e1b561d83b70162efe1624b16
Paulappz <pauloluyege@gmail.com>
2023-07-25 08:42:48 +0100

    feature:quote web

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,47 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const [quote, setQuote] = useState({ quote: "", by: "" });
+
+  // Function to collect data
+
+  const getApiData = async () => {
+    const response = await fetch(
+      "http://localhost:3000/api/randomquote"
+    ).then(response => response.json())
+    const q = response.quote;
+    setQuote({ quote: q.quote, by: q.by });
+  };
+
+  useEffect(() => {
+    getApiData();
+  }, [setQuote]);
+
+  const handleClick = () => {
+    // update the books state property by adding a new book
+    getApiData();
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <div className="jumbotron">
+          <h1 className="display-4">Quotes</h1>
+        </div>
+        <div className="card">
+          <div className="card-header">
+            <q> <b> {quote.quote}</b> </q>
+            <br />
+
+            by : {quote.by}
+          </div>
+        </div>
+        <button className="text-center" onClick={handleClick}>Get Quote</button>
+      </div>
     </div>
   );
 }
